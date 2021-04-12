a587fd0f2e090835125f79a26e3e92dcb9555210
Gary Ou <gou004@wch136-24.cs.ucr.edu>
2021-04-11 20:01:18 -0700

    rip

--- source/main.c
+++ source/main.c
@@ -1,23 +1,15 @@
-/*	Author: gou004
- *  Partner(s) Name: Partners name [none]: Jo Smith
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#endif
+#endif	
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
+DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as out
+unsigned char temp_input = 0x00;
+while(1) {
+	temp_input = PINA;
+	PORTB = temp_input;
+}
+return 1;
 }
